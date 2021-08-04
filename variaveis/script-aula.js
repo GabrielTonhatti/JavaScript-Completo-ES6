2f3a827dabfcc612314d57a101bd98dff421fcbd
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-08-04 13:21:02 -0300

    First Commit

---
+++ variaveis/script-aula.js
@@ -0,0 +1,30 @@
+var nome = 'Gabriel';
+var idade = 20;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Tonhatti",
+    cidade = "Franca";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+// var time = "Vasco";
+// time = "Flamengo";
+// console.log(time)
+
---
+++ variaveis/script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = "Gabriel";
+
+// Declarar uma variável com a sua idade
+var idade = 20;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atrbuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var time = 'Vasco',
+    professor,
+    pais,
+    endereco,
+    rua;
+
+console.log(nome, idade, comidaFavorita, rua);
